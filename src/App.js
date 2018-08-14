97ff42187604728f33bef6e60fda2d5a8b00567b
M150 <stevovic@gmai.com>
2018-08-14 02:46:23 +0200

    Contact and Header components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        <Contact name="Kan Smith" email="kara@gmail.com" phone="333-333-3333" />
       </div>
     );
   }

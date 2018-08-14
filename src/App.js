777b2d02d8e137ca4738d3b8b6458ae838d6f155
M150 <stevovic@gmai.com>
2018-08-14 03:54:21 +0200

    bootstrap, navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
 class App extends Component {
   render() {
     return (
